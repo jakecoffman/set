928ea59bd83bd660f08eb293fc12544656df9542
jake <jake@jakecoffman.com>
2018-07-19 17:09:36 -0500

    update images

--- config/index.js
+++ config/index.js
@@ -12,14 +12,14 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/ws': {
-        target: 'http://localhost:8222',
+        target: 'http://127.0.0.1:8222',
         changeOrigin: true,
         ws: true
       }
     },
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '127.0.0.1', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
